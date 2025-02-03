03ce34285fc06a7e7f2ad4d7f45cb1bf76de6e47
ViniciusCestarii <viniciuscestari01@gmail.com>
2025-02-03 09:37:50 -0300

    refact: remove unnused utils

--- pkg/utils/helper.go
+++
@@ -1,6 +0,0 @@
-package utils
-
-// Length of message (including length itself = 4)
-func AppendMessageLength(message []byte) []byte {
-	return append(Int32ToBytes(int32(len(message)+4)), message...)
-}
